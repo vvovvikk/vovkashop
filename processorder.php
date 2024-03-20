f5dbee7c2af08ab02de193cc53d9c9d394434607
vvovvikk <coin@mok.by>
2024-03-20 14:50:28 +0300

    first commit

---
+++ processorder.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Автозапчасти от Вовки</title>
+</head>
+
+<body>
+    <h1>Автозапчасти от Вовки</h1>
+    <h2>Результаты заказа</h2>
+
+    <?php
+    echo "<p>Заказ обработан в ".date('H:i, jS F Y')."</p>";
+    ?>
+</body>
+
+</html>
